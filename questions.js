7c0643a78d996bf3174603521c9ae6b28dcd1806
Sathyakumar <93641901+Febiecode@users.noreply.github.com>
2022-10-07 00:38:10 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "django is written in which language?",
+    answer: "Python",
+    options: [
+      "Python",
+      "Java",
+      "PHP",
+      "C Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Django is a type of",
+    answer: "Web framework",
+    options: [
+      "Tool",
+      "Software",
+      "Web framework",
+      "Programming Language"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Django is a _____________ Python web framework.",
+    answer: "High-Level",
+    options: [
+      "High-Level",
+      "Mid-Level",
+      "Low-Level",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which architectural pattern does django follow?",
+    answer: "MVT",
+    options: [
+      "CSS",
+      "MVC",
+      "MVT",
+      "PHP"
+    ]
+  },
+    {
+    numb: 5,
+    question: "MVT Stands for?",
+    answer: "Model-View-Template",
+    options: [
+      "Model-View-Table",
+      "Map-View-Template",
+      "Model-View-Template",
+      "Main-View-Template"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
